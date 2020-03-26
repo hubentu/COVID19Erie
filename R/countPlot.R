f0893e5de5467d1d4ff0558f48de86a843679d42
hubuntu <hq@live.cn>
2020-03-25 23:46:49 -0400

    update curve

--- R/countPlot.R
+++ R/countPlot.R
@@ -11,12 +11,15 @@ countPlot <- function(historyCount, update = FALSE){
     if(update){
         if(!as.character(Sys.Date()) %in% historyCount$date){
             counts <- caseCounts()
-            current <- colSums(counts[,-1])
-            historyCount <- rbind(historyCount,
-                                  data.frame(date = as.character(Sys.Date()), rbind(current)))
+            ## current <- colSums(counts[,-1])
+            attr <- attributes(counts)
+            current <- data.frame(date = as.character(Sys.Date()), rbind(c(attr$total.confirmed, attr$total.recovered, attr$total.deaths)))
+            colnames(current) <- colnames(historyCount)
+            historyCount <- rbind(historyCount, current)                      
             return(historyCount)
         }
     }
+    historyCount$active <- historyCount$confirmed - historyCount$recovered - historyCount$deaths
     hcounts <- historyCount %>% pivot_longer(-1, names_to="group", values_to="count") %>%
         mutate(group = fct_relevel(group, "confirmed", "recovered", "deaths"))
     p <- ggplot(hcounts, aes(x = date, y = count, group = group, colour = group)) +
