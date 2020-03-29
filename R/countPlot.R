80503e422b1e1aa19e2b9cabef85925282acd949
hubuntu <hq@live.cn>
2020-03-29 16:11:51 -0400

    update count plots

--- R/countPlot.R
+++ R/countPlot.R
@@ -20,11 +20,23 @@ countPlot <- function(historyCount, update = FALSE){
         }
     }
     historyCount$active <- historyCount$confirmed - historyCount$recovered - historyCount$deaths
-    hcounts <- historyCount %>% pivot_longer(-1, names_to="group", values_to="count") %>%
-        mutate(group = fct_relevel(group, "confirmed", "recovered", "deaths"))
+    historyCount$ycount <- c(0, historyCount$confirmed[-nrow(historyCount)])
+    hcounts <- historyCount %>%
+        mutate(active = confirmed - recovered - deaths,
+               new = confirmed - ycount) %>%
+        select(-ycount) %>%
+        pivot_longer(-1, names_to="group", values_to="count") %>%
+        mutate(group = fct_relevel(group, "confirmed", "active", "new", "recovered", "deaths"))
     p <- ggplot(hcounts, aes(x = date, y = count, group = group, colour = group)) +
         geom_line() + geom_point() +
         theme_bw() +
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    
+    ## hcounts <- historyCount %>% pivot_longer(-1, names_to="group", values_to="count") %>%
+    ##     mutate(group = fct_relevel(group, "confirmed", "recovered", "deaths"))
+    ## p <- ggplot(hcounts, aes(x = date, y = count, group = group, colour = group)) +
+    ##     geom_line() + geom_point() +
+    ##     theme_bw() +
+    ##     theme(axis.text.x = element_text(angle = 45, hjust = 1))
     ggplotly(p)
 }
