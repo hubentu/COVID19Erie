5e0439e9f5f4b30fb8af41783e64ac8c94eee0bf
hubuntu <hq@live.cn>
2020-04-03 23:40:29 -0400

    add logscale to count plot

--- R/countPlot.R
+++ R/countPlot.R
@@ -40,19 +40,33 @@ countPlot <- function(historyCount, update = FALSE){
     ##     pivot_longer(-1, names_to="group", values_to="count") %>%
     ##     mutate(group = fct_relevel(group, "confirmed", "active", "new", "recovered", "deaths"))
 
-    hcounts <- historyCount %>%
-                pivot_longer(-1, names_to="group", values_to="count") %>%
-                mutate(group = fct_relevel(group, "confirmed", "new", "active", "recovered", "deaths"))
-    p <- ggplot(hcounts, aes(x = date, y = count, group = group, colour = group)) +
-        geom_line() + geom_point() +
-        theme_bw() +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1))
-    
-    ## hcounts <- historyCount %>% pivot_longer(-1, names_to="group", values_to="count") %>%
-    ##     mutate(group = fct_relevel(group, "confirmed", "recovered", "deaths"))
+    ## hcounts <- historyCount %>%
+    ##             pivot_longer(-1, names_to="group", values_to="count") %>%
+    ##             mutate(group = fct_relevel(group, "confirmed", "new", "active", "recovered", "deaths"))
     ## p <- ggplot(hcounts, aes(x = date, y = count, group = group, colour = group)) +
     ##     geom_line() + geom_point() +
     ##     theme_bw() +
     ##     theme(axis.text.x = element_text(angle = 45, hjust = 1))
-    ggplotly(p)
+    ## ggplotly(p)
+
+    fig <- plot_ly(historyCount, x = ~date, y = ~confirmed, name = 'confirmed', type = 'scatter', mode = 'lines+markers') %>%
+        add_trace(y = ~active, name = 'active', mode = 'lines+markers') %>%
+        add_trace(y = ~recovered, name = 'recovered', mode = 'lines+markers') %>%
+        add_trace(y = ~new, name = 'new', mode = 'lines+markers') %>%
+        add_trace(y = ~deaths, name = 'deaths', mode = 'lines+markers')
+    
+    fig <- fig %>%
+        layout(yaxis = list(title = "counts"),
+               updatemenus = list(list(
+                   active = 0,
+                   buttons= list(
+                       list(label = 'linear',
+                            method = 'update',
+                            args = list(list(visible = c(T,T,T,T,T)),
+                                        list(yaxis = list(type = 'linear')))),
+                       list(label = 'log',
+                            method = 'update', 
+                            args = list(list(visible = c(T,T,T,T,T)),
+                                        list(yaxis = list(type = 'log'))))))))
+    fig
 }
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -11,3 +11,23 @@ report <- caseReport(counts, pubExposed, historyCount)
 htmlwidgets::saveWidget(report, "index.html")
 
 rmarkdown::render("vignettes/dashboard.Rmd")
+
+fig <- plot_ly(historyCount, x = ~date, y = ~confirmed, name = 'confirmed', type = 'scatter', mode = 'lines+markers') %>%
+    add_trace(y = ~active, name = 'active', mode = 'lines+markers') %>%
+    add_trace(y = ~recovered, name = 'recovered', mode = 'lines+markers') %>%
+    add_trace(y = ~new, name = 'new', mode = 'lines+markers') %>%
+    add_trace(y = ~deaths, name = 'deaths', mode = 'lines+markers')
+
+fig <- fig %>%
+    layout(yaxis = list(title = "counts"),
+        updatemenus = list(list(
+               active = 0,
+               buttons= list(
+                   list(label = 'linear',
+                        method = 'update',
+                        args = list(list(visible = c(T,T,T,T,T)),
+                                    list(yaxis = list(type = 'linear')))),
+                   list(label = 'log',
+                        method = 'update', 
+                        args = list(list(visible = c(T,T,T,T,T)),
+                                    list(yaxis = list(type = 'log'))))))))
