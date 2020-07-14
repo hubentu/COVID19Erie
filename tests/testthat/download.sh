69a8b7709a3f4ade5b6897bb1a0b2a6520b46776
hubentu <qiang.hu@roswellpark.org>
2020-07-14 07:40:05 -0400

    update

--- tests/testthat/download.sh
+++ tests/testthat/download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm erie.html
-./save_page_as "https://erieny.maps.arcgis.com/apps/opsdashboard/index.html#/dd7f1c0c352e4192ab162a1dfadc58e1" --load-wait-time 20 --save-wait-time 5 -d erie.html
+`dirname "$0"`/save_page_as "https://erieny.maps.arcgis.com/apps/opsdashboard/index.html#/dd7f1c0c352e4192ab162a1dfadc58e1" --load-wait-time 20 --save-wait-time 5 -d erie.html
 rm -r erie_files
